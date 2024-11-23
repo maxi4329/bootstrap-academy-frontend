1ecc05ecbe85e9a1c431e4721749b7be7abe87a1
maxi4329 <maxistrasser08@gmail.com>
2024-11-23 18:43:40 +0100

    changed tab size to 2 spaces

--- components/ThemeSelector.vue
+++ components/ThemeSelector.vue
@@ -1,14 +1,14 @@
 <template>
-    <section class="inline-block mr-4">
-        <button>
-            <svg xmlns="http://www.w3.org/2000/svg" height="80" viewBox="40 40 220 120">
-                <circle cx="100" cy="100" r="60" fill="var(--color-accent)" />
-                <circle cx="150" cy="100" r="60" fill="var(--color-secondary)" />
-                <circle cx="200" cy="100" r="60" fill="var(--color-primary)" />
-            </svg>
+  <section class="inline-block mr-4">
+    <button>
+      <svg xmlns="http://www.w3.org/2000/svg" height="80" viewBox="40 40 220 120">
+        <circle cx="100" cy="100" r="60" fill="var(--color-accent)" />
+        <circle cx="150" cy="100" r="60" fill="var(--color-secondary)" />
+        <circle cx="200" cy="100" r="60" fill="var(--color-primary)" />
+      </svg>
 
-        </button>
-    </section>
+    </button>
+  </section>
 </template>
 
 <script lang="ts">
@@ -16,10 +16,11 @@ import { defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
-    setup() {
-        const { t } = useI18n();
-        return { t };
-    },
+
+  setup() {
+    const { t } = useI18n();
+    return { t };
+  },
 });
 </script>
 
--- components/account/ChangeThemes.vue
+++ components/account/ChangeThemes.vue
@@ -1,25 +1,25 @@
 <template>
-    <article class="bg-secondary card style-card flex flex-col items-center justify-center">
-        <PaintBrushIcon class="h-10 w-10 text-accent mb-4 max-w-xl" />
+  <article class="bg-secondary card style-card flex flex-col items-center justify-center">
+    <PaintBrushIcon class="h-10 w-10 text-accent mb-4 max-w-xl" />
 
-        <h2 class="text-heading-2">
-            {{ t("Headings.Themes") }}
-        </h2>
+    <h2 class="text-heading-2">
+      {{ t("Headings.Themes") }}
+    </h2>
 
-        <div class="flex items-center space-x-4 mt-2 mb-8">
-            <CheckCircleIcon class="h-8 w-8 text-accent max-w-xl" />
+    <div class="flex items-center space-x-4 mt-2 mb-8">
+      <CheckCircleIcon class="h-8 w-8 text-accent max-w-xl" />
 
-            <div class="flex flex-col items-center">
-                <p class="text-center">
-                    {{ t("Body.AdjustTheDesign") }}
-                </p>
-            </div>
-        </div>
+      <div class="flex flex-col items-center">
+        <p class="text-center">
+          {{ t("Body.AdjustTheDesign") }}
+        </p>
+      </div>
+    </div>
 
-        <NuxtLink to="/account/themes">
-            <Btn>{{ t('Buttons.OpenThemeSettings') }}</Btn>
-        </NuxtLink>
-    </article>
+    <NuxtLink to="/account/themes">
+      <Btn>{{ t('Buttons.OpenThemeSettings') }}</Btn>
+    </NuxtLink>
+  </article>
 </template>
 
 <script lang="ts">
@@ -29,16 +29,16 @@ import { defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
-    components: {
-        CheckCircleIcon,
-        ShieldExclamationIcon,
-        PaintBrushIcon
-    },
-    setup() {
-        const { t } = useI18n();
+  components: {
+    CheckCircleIcon,
+    ShieldExclamationIcon,
+    PaintBrushIcon
+  },
+  setup() {
+    const { t } = useI18n();
 
-        return { t };
-    },
+    return { t };
+  },
 });
 </script>
 
